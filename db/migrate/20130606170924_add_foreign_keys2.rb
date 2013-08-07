8b0644a647211a4ba226df32e5e4b10334d9b47c
James Schumann <jschumann@gmail.com>
2013-08-07 08:57:35 -0400

    removed foreign key which was failing migration

--- db/migrate/20130606170924_add_foreign_keys2.rb
+++ db/migrate/20130606170924_add_foreign_keys2.rb
@@ -10,7 +10,7 @@ class AddForeignKeys2 < ActiveRecord::Migration
     add_foreign_key_if_not_exists :calendar_events, :calendar_events, :column => :parent_calendar_event_id, :delay_validation => true
     add_foreign_key_if_not_exists :collaborations, :users, :delay_validation => true
     add_foreign_key_if_not_exists :collaborators, :collaborations, :delay_validation => true
-    add_foreign_key_if_not_exists :communication_channels, :users, :delay_validation => true
+    #add_foreign_key_if_not_exists :communication_channels, :users, :delay_validation => true
     add_foreign_key_if_not_exists :content_exports, :content_migrations, :delay_validation => true
     add_foreign_key_if_not_exists :content_exports, :courses, :delay_validation => true
     add_foreign_key_if_not_exists :content_migrations, :courses, :column => :source_course_id, :delay_validation => true
@@ -20,7 +20,7 @@ class AddForeignKeys2 < ActiveRecord::Migration
     remove_foreign_key_if_exists :content_migrations, :column => :source_course_id
     remove_foreign_key_if_exists :content_exports, :courses
     remove_foreign_key_if_exists :content_exports, :content_migrations
-    remove_foreign_key_if_exists :communication_channels, :users
+    #remove_foreign_key_if_exists :communication_channels, :users
     remove_foreign_key_if_exists :collaborators, :collaborations
     remove_foreign_key_if_exists :collaborations, :users
     remove_foreign_key_if_exists :calendar_events, :column => :parent_calendar_event_id
