2ad062c9da0028403eb232612507fe2e53cb9758
James Schumann <jschumann@gmail.com>
2013-08-08 15:03:59 -0400

    grrrr. migrations.

--- db/migrate/20130808000000_add_notified_at_to_context_module_progressions.rb
+++ db/migrate/20130808000000_add_notified_at_to_context_module_progressions.rb
@@ -2,10 +2,10 @@ class AddNotifiedAtToContextModuleProgressions < ActiveRecord::Migration
   tag :predeploy
 
   def self.up
-    add_column :notified_at, :context_module_progressions, :datetime
+    add_column :context_module_progressions, :notified_at, :datetime
   end
 
   def self.down
-    remove_column :notified_at, :context_module_progressions
+    remove_column :context_module_progressions, :notified_at
   end
 end
